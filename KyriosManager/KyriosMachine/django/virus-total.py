ff62417b3ba133315709e1a74cd249d80efe2e02
JoJoseB <bezerra909@gmail.com>
2024-08-28 17:24:58 -0300

    Utilizacao VirusTotalApi - Issue #16

--- KyriosManager/KyriosMachine/django/virus-total.py
+++ KyriosManager/KyriosMachine/django/virus-total.py
@@ -1,32 +1,58 @@
 import os
 import requests
-import json
+from datetime import datetime
+import time
 
-url = "https://www.virustotal.com/api/v3/files/upload_url"
+def VT_api(file):
+    #Url para requisicao da URL de upload
+    url = "https://www.virustotal.com/api/v3/files/upload_url"
 
-headers = {
-    "accept": "application/json",
-    "x-apikey": os.environ.get("VT_API")  
-}
+    #Header file com os dados do arquivo de analise
+    files = { "file": ("teste", open(file, "rb"), "application/vnd.android.package-archive") }
 
-response = (requests.get(url, headers=headers)).json()
+    #Demais inforamcoes do header com a chame api sendo um variavel de ambiente
+    headers = {
+        "accept": "application/json",
+        "x-apikey": os.environ.get("VT_API")  #Chave api variavel de ambiente com nome de VT_API
+    }
 
-files = { "file": ("teste", open("/home/jose/Downloads/com.spike.old.apk", "rb"), "application/vnd.android.package-archive") }
+    #Requisicao da URL de upload
+    response = (requests.get(url, headers=headers)).json()
 
-response2 = (requests.post(response["data"],files=files,headers=headers)).json()
+    #Upload do arquivos
+    response = (requests.post(response['data'],files=files,headers=headers)).json()
 
-print(response2['data']['links']['self'])
+    #As vezes a requisicao pode demorar um pouco pra ficar pronto
+    time.sleep(5)
+ 
+    #Requisicao da situacao da analise
+    response = (requests.get(response['data']['links']['self'],headers=headers)).json()
 
-print("="*100)
+    #Resultado da analise
+    response = (requests.get(response['data']['links']['item'],headers=headers)).json()
 
-response3 = (requests.get(response2['data']['links']['self'],headers=headers)).json()
-
-with open('response3','w') as f:
-    f.write(str(response3))
-
-print("="*100)
-
-response4 = (requests.get(response3['data']['links']['item'],headers=headers)).json()
-
-with open('response4','w') as f:
-    f.write(str(response4))
+    #Categorias atribuidas ao arquivo
+    categoria_ameaca = response["data"]['attributes']['popular_threat_classification']['popular_threat_category']
+    
+    #Cassificacoes atribuidas ao arquivo
+    classificacao_ameaca = response["data"]['attributes']["last_analysis_stats"]
+    
+    #Resultados de engines de antivirus
+    antivirus_resultados = response["data"]['attributes']["last_analysis_results"]
+    
+    #Nivel de Ameaca para a rede
+    rede_ameaca = response["data"]['attributes']["crowdsourced_ids_stats"]
+    
+    #Primeiro avistamento
+    primeiro_avistamento = (datetime.fromtimestamp(response["data"]['attributes']["first_submission_date"])).strftime('%Y-%m-%d %H:%M:%S')
+    
+    #Contexto de rede (IP,URLs,Hosts) cadastrado por IDS
+    tam_res = len(response["data"]['attributes']["crowdsourced_ids_results"])
+    contexto_rede = []
+    for a in range(tam_res):
+        if response["data"]['attributes']["crowdsourced_ids_results"][a]["alert_context"] in contexto_rede:
+            pass
+        else:
+            contexto_rede.append(response["data"]['attributes']["crowdsourced_ids_results"][a]["alert_context"])
+    print(contexto_rede)
+    return {'categoria_ameaca':categoria_ameaca,'classificacao_ameaca':classificacao_ameaca,'antivirus_resultados':antivirus_resultados,'rede_ameaca':rede_ameaca, 'primeiro_avistamento':primeiro_avistamento }
